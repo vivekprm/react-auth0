6b9a4f84cfaff50c73607b0d6d5ce626dac8e930
Vivek Mishra <vivek.mishra@cdk.com>
2019-10-25 17:44:23 +0530

    Created APIs protected by scope and role

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const jwt = require("express-jwt"); // Validate JWT and set req.user
 const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint that Auth0 exposes for our domain.
+const checkScope = require("express-jwt-authz"); // Validate JWT scopes
 
 const app = express();
 
@@ -32,6 +33,32 @@ app.get("/public", function(req, res) {
 app.get("/private", checkJwt, function(req, res) {
   res.json({
     message: "Hello from a private API."
+  });
+});
+
+app.get("/courses", checkJwt, checkScope(["read:courses"]), function(req, res) {
+  res.json({
+    courses: [
+      { id: 1, title: "Building app with react and redux" },
+      { id: 2, title: "Creating reusable react components." }
+    ]
+  });
+});
+
+function checkRole(role) {
+  return function(req, res, next) {
+    const assignedRoles = req.user["http://localhost:3000/roles"];
+    if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+      return next();
+    } else {
+      res.status(401).send("Insufficient role");
+    }
+  };
+}
+
+app.get("/admin", checkJwt, checkRole("admin"), function(req, res) {
+  res.json({
+    message: "Hello from a admin API."
   });
 });
 
